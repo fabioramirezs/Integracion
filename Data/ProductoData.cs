d219f667e5eae19b6b370c8afecd2776771e3e9a
Fabio Ramirez <fabioalexander.ramirez@gmail.com>
2024-06-13 22:58:22 -0500

    Agregar costo a Producto, ProductoDto y COntroller

--- Data/ProductoData.cs
+++ Data/ProductoData.cs
@@ -10,10 +10,10 @@ namespace Data
     {
         static public List<Producto> listaProducto = new List<Producto>
         {
-            new Producto {Id=1, NombreProducto="Producto 1", Categoria="Computadoras", Marca="HP", Precio=2000},
-            new Producto {Id=2, NombreProducto="Producto 2", Categoria="Computadoras", Marca="Asus", Precio=1800},
-            new Producto {Id=3, NombreProducto="Producto 3", Categoria="Computadoras", Marca="Dell", Precio=2500},
-            new Producto {Id=4, NombreProducto="Producto 4", Categoria="Computadoras", Marca="Lenovo", Precio=2800},
+            new Producto {Id=1, NombreProducto="Producto 1", Categoria="Computadoras", Marca="HP", Precio=2000, Costo=1880},
+            new Producto {Id=2, NombreProducto="Producto 2", Categoria="Computadoras", Marca="Asus", Precio=1800, Costo=1580},
+            new Producto {Id=3, NombreProducto="Producto 3", Categoria="Computadoras", Marca="Dell", Precio=2500, Costo=2480},
+            new Producto {Id=4, NombreProducto="Producto 4", Categoria="Computadoras", Marca="Lenovo", Precio=2800, Costo=2580},
 
         };
     }
