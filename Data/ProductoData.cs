9d6f891c157dd78b80c40996340a21179c8a6026
Fabio Ramirez <fabioalexander.ramirez@gmail.com>
2024-06-25 14:38:19 -0500

    Agragar endpoints para Producto

--- Data/ProductoData.cs
+++ Data/ProductoData.cs
@@ -8,13 +8,13 @@ namespace Data
 {
     public static class ProductoData
     {
-        static public List<Producto> listaProducto = new List<Producto>
-        {
-            new Producto {Id=1, NombreProducto="Producto 1", Categoria="Computadoras", Marca="HP", Precio=2000, Costo=1880},
-            new Producto {Id=2, NombreProducto="Producto 2", Categoria="Computadoras", Marca="Asus", Precio=1800, Costo=1580},
-            new Producto {Id=3, NombreProducto="Producto 3", Categoria="Computadoras", Marca="Dell", Precio=2500, Costo=2480},
-            new Producto {Id=4, NombreProducto="Producto 4", Categoria="Computadoras", Marca="Lenovo", Precio=2800, Costo=2580},
+        // static public List<Producto> listaProducto = new List<Producto>
+        // {
+        //     new Producto {Id=1, NombreProducto="Producto 1", CategoriaId=1, MarcaId=1, Precio=2000, Costo=1880},
+        //     new Producto {Id=2, NombreProducto="Producto 2", CategoriaId=1, MarcaId=1, Precio=1800, Costo=1580},
+        //     new Producto {Id=3, NombreProducto="Producto 3", CategoriaId=2, MarcaId=2, Precio=2500, Costo=2480},
+        //     new Producto {Id=4, NombreProducto="Producto 4", CategoriaId=2, MarcaId=2, Precio=2800, Costo=2580},
 
-        };
+        // };
     }
 }
